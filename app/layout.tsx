331c28bd016951a45af836e727d5f1f1071714e4
wasupon <wasupon.mail@gmail.com>
2024-07-06 19:25:15 +0900

    fix: favicon ico sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className="relative">
         <Navbar />
         {children}
