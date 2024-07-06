948692e097cde1c3f4fae704767c0aecc6d0b4bc
wasupon <wasupon.mail@gmail.com>
2024-07-06 19:28:37 +0900

    fix: move icon.svg to app folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className="relative">
         <Navbar />
         {children}
